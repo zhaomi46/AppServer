6e5cffc95e55fe426de6420b3b4aa8ae4d983a33
zzl <726672495@qq.com>
2019-05-23 18:10:15 +0800

    Task update

--- swsad/src/main/java/com/zgl/swsad/model/Questionare.java
+++ swsad/src/main/java/com/zgl/swsad/model/Questionare.java
@@ -1,4 +1,46 @@
 package com.zgl.swsad.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
 public class Questionare {
+    private Integer questionareId;
+    private Integer questionNum;
+    private Integer taskId;
+
+    public Integer getQuestionareId() {
+        return questionareId;
+    }
+
+    public void setQuestionareId(Integer questionareId) {
+        this.questionareId = questionareId;
+    }
+
+    public Integer getQuestionNum() {
+        return questionNum;
+    }
+
+    public void setQuestionNum(Integer questionNum) {
+        this.questionNum = questionNum;
+    }
+
+    public Integer getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Integer taskId) {
+        this.taskId = taskId;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+
+    @Autowired
+    private ArrayList<Question> questions;
 }
