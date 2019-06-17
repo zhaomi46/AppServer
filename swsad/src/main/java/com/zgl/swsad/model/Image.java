4411ef2c5b24eec026e74b22f3d231131a2482da
ArtemisZGL <870060239@qq.com>
2019-06-17 11:23:28 +0800

    add image name in Image module

---
+++ swsad/src/main/java/com/zgl/swsad/model/Image.java
@@ -0,0 +1,18 @@
+package com.zgl.swsad.model;
+
+public class Image {
+    public Image(String imageName)
+    {
+        this.imageName = imageName;
+    }
+
+    private String imageName;
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String publishTime) {
+        this.imageName = publishTime;
+    }
+}
