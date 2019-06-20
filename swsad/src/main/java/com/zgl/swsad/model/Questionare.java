e94d800b486d252ce123b2a50b8f15ee587294e9
zzl <726672495@qq.com>
2019-06-20 18:33:27 +0800

    Update Questionare.java

--- swsad/src/main/java/com/zgl/swsad/model/Questionare.java
+++ swsad/src/main/java/com/zgl/swsad/model/Questionare.java
@@ -8,6 +8,18 @@ public class Questionare {
     private Integer questionareId;
     private Integer questionNum;
     private Integer taskId;
+    private String description;
+    private String title;
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+
 
     public Integer getQuestionareId() {
         return questionareId;
