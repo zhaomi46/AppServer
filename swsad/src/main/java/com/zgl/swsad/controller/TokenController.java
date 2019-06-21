824a7f12672ee11431d49a7b7cb032740ec1e9fd
dlock_z <dlz@email.com>
2019-06-21 14:04:43 +0800

    update tokens

--- swsad/src/main/java/com/zgl/swsad/controller/TokenController.java
+++ swsad/src/main/java/com/zgl/swsad/controller/TokenController.java
@@ -1,11 +1,13 @@
 package com.zgl.swsad.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.zgl.swsad.authorization.annotation.Authorization;
 import com.zgl.swsad.authorization.annotation.CurrentUser;
 import com.zgl.swsad.authorization.manager.TokenManager;
 import com.zgl.swsad.authorization.model.TokenModel;
 import com.zgl.swsad.model.User;
 import com.zgl.swsad.service.UserService;
+import com.zgl.swsad.util.ReturnMsg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +24,21 @@ public class TokenController {
     @Autowired
     private TokenManager tokenManager;
 
+//    @CrossOrigin
+//    @RequestMapping(method = RequestMethod.POST)
+//    public ResponseEntity<Object> login(@RequestParam String username, @RequestParam String password) throws Exception {
+//        Assert.notNull(username, "username can not be empty");
+//        Assert.notNull(password, "password can not be empty");
+//
+//        User user = userRepository.selectUserByname(username);
+//        if(user == null || !user.getPassword().equals(password))
+//            return new ResponseEntity("invlid username/password", HttpStatus.UNAUTHORIZED);
+//        //生成一个token，保存用户登录状态
+//        String tokenString = tokenManager.createToken(user.getUserId().longValue(), true);
+//        return new ResponseEntity(tokenString, HttpStatus.OK);
+//    }
     @CrossOrigin
+    @ResponseBody
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Object> login(@RequestParam String username, @RequestParam String password) throws Exception {
         Assert.notNull(username, "username can not be empty");
@@ -33,7 +49,18 @@ public class TokenController {
             return new ResponseEntity("invlid username/password", HttpStatus.UNAUTHORIZED);
         //生成一个token，保存用户登录状态
         String tokenString = tokenManager.createToken(user.getUserId().longValue(), true);
-        return new ResponseEntity(tokenString, HttpStatus.OK);
+        Integer userID = user.getUserId();
+        JSONObject result = new JSONObject();
+        result.put("useId", userID);
+        result.put("token", tokenString);
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET)
+    @Authorization
+    public ResponseEntity<Object> check(@CurrentUser User user){
+        return new ResponseEntity(new ReturnMsg("ok"), HttpStatus.OK);
     }
 
     @CrossOrigin
